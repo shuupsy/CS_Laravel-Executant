7af59a472d1e9f9f0924d8ea1b584ec8c95d4c08
shuupsy <sylvaine.ruan@gmail.com>
2022-09-27 11:02:31 +0200

    Avatars, Images, Users : Model / Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->boolean('is_Admin')
+                ->default(0);
             $table->string('name');
+            $table->string('first_name');
+            $table->unsignedBigInteger('age');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('avatar_id')
+                ->default(1)
+                ->constrained()
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
